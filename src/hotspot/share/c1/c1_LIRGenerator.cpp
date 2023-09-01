b46c15823388403f2082bd1081fc52f1d836c83a
Aleksey Shipilev <shipilev@amazon.de>
2023-09-01 20:52:37 +0200

    Do the cmove again

--- src/hotspot/share/c1/c1_LIRGenerator.cpp
+++ src/hotspot/share/c1/c1_LIRGenerator.cpp
@@ -923,22 +923,18 @@ void LIRGenerator::profile_branch(If* if_instr, If::Condition cond) {
       not_taken_count_offset = t;
     }
 
-    int diff = not_taken_count_offset - taken_count_offset;
+    LIR_Opr md_taken_slot = LIR_OprFact::intptrConst(((char *) md->constant_encoding()) + taken_count_offset);
+    LIR_Opr md_not_taken_slot = LIR_OprFact::intptrConst(((char *) md->constant_encoding()) + not_taken_count_offset);
 
-    LIR_Opr data_loc_reg = new_pointer_register();
-    LIR_Opr taken_addr = LIR_OprFact::intptrConst(((char*)md->constant_encoding()) + taken_count_offset);
-
-    __ move(taken_addr, data_loc_reg);
-
-    LabelObj* L = new LabelObj();
-    __ branch(lir_cond(cond), L->label());
-    LIR_Address* fake_incr_value2 = new LIR_Address(data_loc_reg, diff, T_INT);
-    __ leal(fake_incr_value2, data_loc_reg);
-    __ branch_destination(L->label());
+    LIR_Opr data_offset_reg = new_pointer_register();
+    __ cmove(lir_cond(cond),
+             md_taken_slot,
+             md_not_taken_slot,
+             data_offset_reg, as_BasicType(if_instr->x()->type()));
 
     // MDO cells are intptr_t, so the data_reg width is arch-dependent.
-    LIR_Address* data_addr = new LIR_Address(data_loc_reg, data_loc_reg->type());
     LIR_Opr data_reg = new_pointer_register();
+    LIR_Address* data_addr = new LIR_Address(data_offset_reg, data_reg->type());
     __ move(data_addr, data_reg);
     // Use leal instead of add to avoid destroying condition codes on x86
     LIR_Address* fake_incr_value = new LIR_Address(data_reg, DataLayout::counter_increment, T_INT);
